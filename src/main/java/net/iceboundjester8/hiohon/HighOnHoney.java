e015824d3584a2283d09b9221cfc3889a1cd71f4
Iceboundjester8 <Iceboundjester8@gmail.com>
2025-01-10 20:57:22 -0600

    Version 0.0.1 - Project Startup

--- src/main/java/net/iceboundjester8/hiohon/HighOnHoney.java
+++ src/main/java/net/iceboundjester8/hiohon/HighOnHoney.java
@@ -8,11 +8,7 @@ import org.slf4j.LoggerFactory;
 public class HighOnHoney implements ModInitializer {
 	public static final String MOD_ID = "highonhoney";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-<<<<<<< HEAD
 
-=======
-//Temp syntax check
->>>>>>> 8930f1b (Version 0.0.1 - Project Startup)
 	@Override
 	public void onInitialize() {
 
